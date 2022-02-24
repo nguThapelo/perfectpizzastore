eb090e39cd48a7194c2e21fa55bfb5e3d3227917
Thapelo Mmakola <mthapelothapza@gmail.com>
2022-02-24 22:14:45 +0200

    rendered card, adding items to cart and removing them

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,8 +2,12 @@ import React, { useState } from 'react';
 // import list
 import '../styles/Body.css';
 
+const PAGE_PRODUCTS = 'products';
+const PAGE_CART = 'cart'
+
 const Body = () => {
     const [cart, setCart] = useState([]);
+    const [page, setPage] = useState(PAGE_PRODUCTS)
 
     const [products] = useState([
         {
@@ -52,47 +56,60 @@ const Body = () => {
 
     const addToCart = (product) => {
         // alert("button works")
-        setCart([...cart, product]);
+        setCart([...cart, {...product}]);
     }
 
-    return (
-        <div className="container">
-            <h1>Products</h1>
-            <button>Go to cart {cart.length}</button>
+    const gotoCart = (nextPage) => {
+        setPage(nextPage)
+    }
 
+    const removeFromCart = (productToRemove) => {
+        setCart(cart.filter(product => product !== productToRemove)
+
+        )}
+
+    const renderProducts = () => (
+        <>
+          <h1>Products</h1>
             <div className="products">
                 {products.map((product, index) => (
-                <div className="product" key={index}>
-                    <h3> {product.name}</h3>
-                    <img src={product.image} alt="" width="150px" />
-                    <h4> {product.cost}</h4>
-                    <button onClick={() => addToCart(product)}>Add To cart</button>
-                </div>
+                    <div className="product" key={index}>
+                        <h3> {product.name}</h3>
+                        <img src={product.image} alt="" width="150px" />
+                        <h4> {product.cost}</h4>
+                        <button onClick={() => addToCart(product)}>Add To cart</button>
+                    </div>
                 ))}
 
             </div>
+        </>
+    )
 
+    const renderCart = () => (
+        <>
+          <h1>Cart</h1>
+        <div className="products">
+                {cart.map((product, index) => (
+                    <div className="product" key={index}>
+                        <h3> {product.name}</h3>
+                        <img src={product.image} alt="" width="150px" />
+                        <h4> {product.cost}</h4>
+                        <button onClick={() => removeFromCart(product)}>Remove From cart</button>
+                    </div>
+                ))}
 
+            </div>
+        
+        
 
+        </>
+    )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return (
+        <div className="container">
+            <button onClick={() => gotoCart(PAGE_CART)}>Go to cart {cart.length}</button>
+            {page === PAGE_PRODUCTS && renderProducts()}
+            {page === PAGE_CART && renderCart()}
 
         </div>
     )
